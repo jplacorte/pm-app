f3a1b9f664dab24fd5904adfb2bde893d4b6c336
John Phillip Lacorte <jaypeelacorte28@gmail.com>
2025-12-10 09:18:00 +0800

    refactor: cleanup and setup ui package to use vue

---
+++ packages/ui/src/Buttons/TextButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  label: string
+  className?: string
+  onClick?: () => void
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <button
+    :class="props.className"
+    @click="props.onClick"
+  >
+    {{ props.label }}
+  </button>
+</template>
